40958a95d1f016abfc48a547ce34f9d6424f49d7
Smruti <lenkasmruti123@gmail.com>
2021-09-22 14:04:41 +0530

    Added: Addition

---
+++ src/main/java/com/Javapractice/Addition.java
@@ -0,0 +1,12 @@
+package com.Javapractice;
+
+public class Addition {
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 =10;
+		int sum;
+		sum = num1 + num2;
+		System.out.println("Sum of these number:" +sum);
+	}
+
+}
